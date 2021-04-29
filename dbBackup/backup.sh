cdd9efc3ac6948f4c453bae5b7833b9d7e0954a8
nxia0004 <nxia0004@student.monash.edu>
2021-04-29 19:10:15 +1000

    customised container do the data and schema backup

--- dbBackup/backup.sh
+++ dbBackup/backup.sh
@@ -30,12 +30,14 @@ if [ "${MYSQL_PASSWORD}" == "**None**" ]; then
   exit 1
 fi
 
-if [ "${S3_IAMROLE}" != "true" ]; then
-  # env vars needed for aws tools - only if an IAM role is not used
-  export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
-  export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
-  export AWS_DEFAULT_REGION=$S3_REGION
-fi
+
+# if [ "${S3_IAMROLE}" != "true" ]; then
+#   # env vars needed for aws tools - only if an IAM role is not used
+
+#   export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
+#   export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
+#   export AWS_DEFAULT_REGION=$S3_REGION
+# fi
 
 MYSQL_HOST_OPTS="-h $MYSQL_HOST -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD"
 DUMP_START_TIME=$(date +"%Y-%m-%dT%H%M%SZ")
@@ -43,6 +45,10 @@ DUMP_START_TIME=$(date +"%Y-%m-%dT%H%M%SZ")
 copy_s3 () {
   SRC_FILE=$1
   DEST_FILE=$2
+
+  export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
+  export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
+  export AWS_DEFAULT_REGION=$S3_REGION
 
   if [ "${S3_ENDPOINT}" == "**None**" ]; then
     AWS_ARGS=""
@@ -53,7 +59,8 @@ copy_s3 () {
   echo "Uploading ${DEST_FILE} on S3..."
 
   #cat $SRC_FILE | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE
-  cat $SRC_FILE | aws s3 cp - s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE
+  aws s3 cp $SRC_FILE s3://$S3_BUCKET/$S3_PREFIX/$DEST_FILE
+  aws s3 ls s3://$S3_BUCKET
   if [ $? != 0 ]; then
     >&2 echo "Error uploading ${DEST_FILE} on S3"
   fi
@@ -92,21 +99,26 @@ else
   echo "Creating schema and dump for ${MYSQLDUMP_DATABASE} from ${MYSQL_HOST}..."
 
   DUMP_FILE="/tmp/dump.sql.gz"
-  #DUMP_SCHEMA_FILE = "/tmp/schema.sql.gz"
+  DUMP_SCHEMA_FILE="/tmp/schema.sql.gz"
   mysqldump -h $MYSQL_HOST -P $MYSQL_PORT -u$MYSQL_USER -p$MYSQL_PASSWORD --skip-lock-tables --single-transaction --all-databases | gzip > $DUMP_FILE
-  #mysqldump $MYSQL_HOST_OPTS --no-data --all-databases | gzip > $DUMP_SCHEMA_FILE
+  mysqldump $MYSQL_HOST_OPTS --no-data --all-databases | gzip > $DUMP_SCHEMA_FILE
+  echo "Dumping successful"
 
   if [ $? == 0 ]; then
     if [ "${S3_FILENAME}" == "**None**" ]; then
       S3_FILE="${DUMP_START_TIME}.dump.sql.gz"
       copy_s3 $DUMP_FILE $S3_FILE
-      #S3_FILE="${DUMP_START_TIME}.schema.sql.gz"
-      #copy_s3 $DUMP_SCHEMA_FILE $S3_FILE
+      echo "1dumping data successful"
+      S3_FILE="${DUMP_START_TIME}.schema.sql.gz"
+      copy_s3 $DUMP_SCHEMA_FILE $S3_FILE
+      echo "1dumping schema successful"
     else
       S3_FILE="${S3_FILENAME}.sql.gz"
       copy_s3 $DUMP_FILE $S3_FILE
-      #S3_FILE="${DUMP_START_TIME}.schema.sql.gz"
-      #copy_s3 $DUMP_SCHEMA_FILE $S3_FILE
+      echo "1dumping data successful"
+      S3_FILE="${DUMP_START_TIME}.schema.sql.gz"
+      copy_s3 $DUMP_SCHEMA_FILE $S3_FILE
+      echo "2dumping successful"
     fi
   else
     >&2 echo "Error creating dump of all databases"
