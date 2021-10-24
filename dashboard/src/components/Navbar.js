6ca3fe448671a4c80570850cebbc8f35179a2943
wangke <wannngkee@gmail.com>
2021-10-24 15:28:04 +0800

    Add links to two env sites on the new nav section

--- dashboard/src/components/Navbar.js
+++ dashboard/src/components/Navbar.js
@@ -1,34 +1,43 @@
-
-import React from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
-import {LinkContainer} from 'react-router-bootstrap'
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const header = () => {
-    return (
-      <Navbar bg="dark" variant="dark" className="nav">
-            <LinkContainer to="/">
-  <Navbar.Brand>Solferino Academy Dashboard</Navbar.Brand>
-  </LinkContainer>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
+  return (
+    <Navbar bg="dark" variant="dark" className="nav">
+      <Container>
         <LinkContainer to="/">
-      <Nav.Link>Home</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/features">
-      <Nav.Link>Features</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/perfomancetesting">
-      <Nav.Link>Performance Testing</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/accessibilitytesting">
-      <Nav.Link>Accessibility Testing</Nav.Link>
-      </LinkContainer>
-      
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-    )
-}
+          <Navbar.Brand>Solferino Academy Dashboard</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/features">
+              <Nav.Link>Features</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/perfomancetesting">
+              <Nav.Link>Performance Testing</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/accessibilitytesting">
+              <Nav.Link>Accessibility Testing</Nav.Link>
+            </LinkContainer>
+            <Nav.Link href="https://staging-sa.actionit.dev/" target="_blank">
+              Staging Site
+            </Nav.Link>
+            <Nav.Link
+              href="https://production-sa.actionit.dev/"
+              target="_blank"
+            >
+              Production Site
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default header
+export default header;
