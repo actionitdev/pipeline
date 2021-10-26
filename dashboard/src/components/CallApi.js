f20efd7eabac23c18b64f09115f71c91ff3452e6
Anmol Sarraf <36872756+AnonSar@users.noreply.github.com>
2021-10-26 19:13:04 +1100

    Update CallApi.js

--- dashboard/src/components/CallApi.js
+++ dashboard/src/components/CallApi.js
@@ -7,6 +7,10 @@ class CallApi {
 	runPerformanceTest(body) {
         return new Promise((resolve) => resolve(axios.post(`${api}/performance`, body)));
     }
+
+    runAccessibiliyTest(body) {
+        return new Promise((resolve) => resolve(axios.post(`${api}/accessibility`, body)))
+    }
 }
 
 export default new CallApi();
